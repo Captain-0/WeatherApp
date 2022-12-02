3cce5e178d1fe051aa1844e44cb8b28ddde4c269
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-02 17:49:47 +0530

    added search text box with search icon

--- main.py
+++ main.py
@@ -19,6 +19,14 @@ root.resizable(False, False)
 
 search_image = ImageTk.PhotoImage(file="images/Untitled-2.png")
 my_image = Label(image=search_image)
-my_image.place(x=0, y=20)
+my_image.place(x=20, y=20)
+
+textfield = Entry(root, justify="center", width=23, font=("poppins", 25, "bold"), bg="#6f79c5", border=0, fg="white")
+textfield.place(x=60, y=50)
+textfield.focus()
+
+search_icon = PhotoImage(file="images/awdadawwdad.png")
+my_image_icon = Button(image=search_icon, borderwidth=0, cursor="hand2", bg="#6f79c5")
+my_image_icon.place(x=440, y=33)
 
 root.mainloop()
