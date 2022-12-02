c0e8d88c4f0cbf96c2ccd5b62bc9dc1d81ac2c05
Captain-0 <sufiyan.in.tech+github@gmail.com>
2022-12-02 17:28:53 +0530

    placed the search text box

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from tkinter import *
+
+from PIL import Image, ImageTk
+
 import tkinter as tk
 from geopy.geocoders import Nominatim
 from tkinter import ttk, messagebox
@@ -11,5 +14,11 @@ root = Tk()
 root.title("WEATHER APP")
 root.geometry("900x500+300+200")
 root.resizable(False, False)
+
+# SEARCH BOX
+
+search_image = ImageTk.PhotoImage(file="images/Untitled-2.png")
+my_image = Label(image=search_image)
+my_image.place(x=0, y=20)
 
 root.mainloop()
